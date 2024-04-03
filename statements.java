69d91c0aa54bda940a4b3e6e249b141ecd602b68
GaiaVi <165931513+GaiaVi@users.noreply.github.com>
2024-04-03 20:09:53 +0300

    Create statements.java

---
+++ statements.java
@@ -0,0 +1,22 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+
+    double eggPrice = 2.50;
+    int money = 3;
+
+    // If eggs cost less than 3 euros
+    // Then buy them
+
+    if (eggPrice < money) {
+      System.out.println("1");
+      System.out.println("2");
+      System.out.println("Buy the eggs");
+    }
+
+    System.out.println("3");
+
+  }
+}
